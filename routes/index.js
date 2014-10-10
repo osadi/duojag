e4d29ccf256b9c06f7d62d6521d7a7bc7943705d
Haugen <tobiashaugen@gmail.com>
2014-10-10 17:58:56 +0200

    Templating and theming.

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  res.render('index', {
+    projectName: "DuoJag",
+    partials: {
+      layout: 'layout'
+    }
+  });
+});
 
+/* Matches page */
+router.get('/match', function(req, res) {
+  res.render('match', {
+    projectName: "DuoJag",
+    partials: {
+      layout: 'layout'
+    }
+  });
+});
 
-  res.render('index', { projectName: "DuoJag" });
+/* Waiting page */
+router.get('/waiting', function(req, res) {
+  res.render('waiting', {
+    projectName: "DuoJag",
+    partials: {
+      layout: 'layout'
+    }
+  });
 });
 
 module.exports = router;
